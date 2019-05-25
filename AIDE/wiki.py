54518877e958c352396ff8620fd25d9d1d331387
thomas83470 <estival.t@hotmail.com>
2019-05-25 10:01:53 +0200

    Requete wiki manuel sans wrapper Terminé

--- AIDE/wiki.py
+++ AIDE/wiki.py
@@ -20,17 +20,28 @@ class RequestWiki:
 
     def get_adress(self):
         geo_json = self.geo.json()
-        geo_json = geo_json['query']['geosearch'][0]['title']
-        print(geo_json)
 
-    def resume(self):
-        resume_json = r.get("https://fr.wikipedia.org/w/api.php?action=query&titles=Boulevard%20Michelet%20(Marseille)&prop=extracts&exsentences=3&format=json&explaintext")
+        # On recupère l'Id de la page'
+        id_geo_json = geo_json['query']['geosearch'][0]['pageid']
+
+        # On récupère le titre
+        title_geo_json = geo_json['query']['geosearch'][0]['title']
+
+        # On formate la chaine de caractère pour l'intégrer dans la requête API
+        title_geo_json = title_geo_json.replace(" ", "%20")
+
+        return title_geo_json, id_geo_json
+
+    def resume(self, url, id_page):
+        resume_json = r.get("https://fr.wikipedia.org/w/api.php?action=query&titles={}&prop=extracts&exsentences=2&format=json&explaintext".format(url))
         resume_json = resume_json.json()
-        resume_json = resume_json['query']['pages'][1]
+        resume_json = resume_json['query']['pages'][str(id_page)]['extract']
 
         print(resume_json)
 
 test = RequestWiki()
 test.geo_search()
-test.get_adress()
-test.resume()
+info_api = test.get_adress()
+
+print(info_api)
+test.resume(url = info_api[0], id_page = info_api[1])
