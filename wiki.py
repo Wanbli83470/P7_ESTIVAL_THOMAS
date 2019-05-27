da8b965fe6a144f7821868da2c80bd4935c88c83
thomas83470 <estival.t@hotmail.com>
2019-05-27 16:43:19 +0200

    Travail PEP8 fichier wiki

--- wiki.py
+++ wiki.py
@@ -3,6 +3,7 @@
 import requests as r
 import json
 
+
 class RequestWiki:
 
     """Class constructor to set the coordinates and the number of sentences of the summary"""
@@ -14,13 +15,15 @@ class RequestWiki:
 
     def geo_search(self):
         """Initialization of the url with geosearch to recover data json"""
-        url = "https://fr.wikipedia.org/w/api.php?action=query&list=geosearch&gscoord={}|{}&gsradius=10000&gslimit=10&format=json".format(self.lat, self.lng)
+        url = "https://fr.wikipedia.org/w/api.php?action=query&list=geosearch&gscoord={}|{}&gsradius=10000&gslimit=10&format=json".\
+            format(self.lat, self.lng)
         self.geo = r.get(url)
         status = self.geo.status_code
         print(status)
         return status
 
     def get_adress(self):
+        """Récupération de l'ID de la page et du titre pour la fonction 'resume' """
         geo_json = self.geo.json()
 
         # On recupère l'Id de la page'
@@ -35,7 +38,9 @@ class RequestWiki:
         return title_geo_json, id_geo_json
 
     def resume(self, url, id_page):
-        resume_json = r.get("https://fr.wikipedia.org/w/api.php?action=query&titles={}&prop=extracts&exsentences=2&format=json&explaintext".format(url))
+        """Configuration de l'URL d'une requête API avec les données pour extraires les phrases importantes """
+        resume_json = r.get("https://fr.wikipedia.org/w/api.php?action=query&titles={}&prop=extracts&exsentences=2&format=json&explaintext".
+                            format(url))
         resume_json = resume_json.json()
         resume_json = resume_json['query']['pages'][str(id_page)]['extract']
 
